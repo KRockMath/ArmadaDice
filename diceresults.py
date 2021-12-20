8b504f8a9b8b1f136e31503fcbc1a1bcc2840849
Kris Erickson <kristopher.erickson@GMAIL.com>
2021-12-20 14:33:35 -0800

    Move and Del some files. New Dice

---
+++ diceresults.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 16 12:01:17 2021
+
+@author: KRockMAth
+Source: https://starwars-armada.fandom.com/wiki/Dice
+"""
+
+#import random
+import numpy as np
+import pandas as pd
+#%%
+
+def eightsided():
+    return np.random.randint(8)
+
+#eightsided()
+#result = eightsided()
+
+#%%
+def bluedie(x):
+    if x <= 4:
+        return 'hit'
+    elif x <= 6:
+        return 'crit'
+    else:
+        return 'accr'
+
+#blueresult = bluedie( eightsided() )
+#print(blueresult)
+
+#%%
+def reddie(x):
+    if x <= 2:
+        return 'hit'
+    elif x <= 4:
+        return 'crit'
+    elif x <= 5:
+        return 'accr'
+    elif x <= 6:
+        return '2hits'
+    else:
+        return 'blnk'    
+    
+#redresult = reddie( eightsided() )
+#print(redresult)    
+    
+#%%
+def blackdie(x):
+    if x <= 4:
+        return 'hit'
+    elif x <= 6:
+        return 'hitcrit'
+    else:
+        return 'blnk'    
+    
+#blackresult = blackdie( eightsided() )
+#print(blackresult)       
+    
+    
+#%%
+def totalresults(blues,reds,blacks):
+    bluelist = []
+    redlist = []
+    blacklist = []
+    i=1
+    while i <= blues:
+        bluelist.append( bluedie(eightsided()) )
+        i = i+1
+    
+    i=1
+    while i <= reds:
+        redlist.append( reddie(eightsided()) )
+        i = i+1
+
+    i=1
+    while i <= blacks:
+        blacklist.append( blackdie(eightsided()) )
+        i = i+1
+    
+    return (bluelist+redlist+blacklist)
+#%%
+#testlist = []    
+testlist = totalresults(1,3,5)    
+#%%
+
+
+def totals(blu,red,blk):
+    testlist = totalresults(blu,red,blk)   
+    totalhits = testlist.count('hit')+2*testlist.count('2hits')+testlist.count('hitcrit') 
+    totalcrits = testlist.count('crit')+testlist.count('hitcrit') 
+    totalblanks = testlist.count('blnk')
+    totalaccuracy = testlist.count('accr') 
+    
+    blargh = []
+    blargh.append(['Hits',totalhits])
+    blargh.append(['Crits',totalcrits])
+    blargh.append(['Blanks',totalblanks])
+    blargh.append(['Accuracy',totalaccuracy])
+    
+    return blargh
+
+#results = totals(2,3,2)
+#df = pd.DataFrame(results)
+#print(df)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ main.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created 2021-12-15
+
+@author: KrockMath
+
+https://pythonspot.com/flask-web-app-with-python/
+https://www.askpython.com/python-modules/flask/flask-forms
+"""
+
+#Flask Tutorial
+
+from flask import Flask,render_template,request
+from flask import jsonify
+#import diceresults.py as dr
+import pandas as pd
+#%%
+app = Flask(__name__)
+
+@app.route("/",methods = ['GET'])
+def main():
+    return '''Welcome to Star Wars Armada Dice Calculator 
+    <br>    
+    <form action="action_to_perform_after_submission" method = "POST">
+        <p>Blue Dice <input type = "text" name = "BlueDice" /></p>
+        <p>Red Dice <input type = "text" name = "RedDice" /></p>
+        <p>Black Dice <input type = "text" name = "BlackDice" /></p>
+        <p><input type = "submit" value = "submit" /></p>
+    </form> 
+     '''
+#%%     
+#@app.route('/action_to_perform_after_submission',methods = ['GET','POST'])
+
+#helpful: https://stackoverflow.com/questions/12277933/send-data-from-a-textbox-into-flask
+def action_to_perform_after_submission(): 
+    if request.method == 'GET':
+        return f"get result"
+    if request.method == 'POST':
+        #results = dr.totals(2,3,2)
+        return "post results:"+str(2)
+        #return f"post result: "+str(jsonify(results)) #didn't show results as a string. just put post resutls
+
+
+#%%   
+if __name__ == "__main__":
+    app.debug = True  #remove this from final
+    app.run(port=8000)
+
