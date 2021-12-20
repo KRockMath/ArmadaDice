d5256da9f51f0bc07e39a9b63e45bc78e857878d
Kris Erickson <kristopher.erickson@GMAIL.com>
2021-12-20 15:46:27 -0800

    Flask form to function

--- diceresults.py
+++ diceresults.py
@@ -40,11 +40,11 @@ def reddie(x):
     elif x <= 6:
         return '2hits'
     else:
-        return 'blnk'    
-    
+        return 'blnk'
+
 #redresult = reddie( eightsided() )
-#print(redresult)    
-    
+#print(redresult)
+
 #%%
 def blackdie(x):
     if x <= 4:
@@ -52,12 +52,12 @@ def blackdie(x):
     elif x <= 6:
         return 'hitcrit'
     else:
-        return 'blnk'    
-    
+        return 'blnk'
+
 #blackresult = blackdie( eightsided() )
-#print(blackresult)       
-    
-    
+#print(blackresult)
+
+
 #%%
 def totalresults(blues,reds,blacks):
     bluelist = []
@@ -67,7 +67,7 @@ def totalresults(blues,reds,blacks):
     while i <= blues:
         bluelist.append( bluedie(eightsided()) )
         i = i+1
-    
+
     i=1
     while i <= reds:
         redlist.append( reddie(eightsided()) )
@@ -77,40 +77,39 @@ def totalresults(blues,reds,blacks):
     while i <= blacks:
         blacklist.append( blackdie(eightsided()) )
         i = i+1
-    
+
     return (bluelist+redlist+blacklist)
 #%%
-#testlist = []    
-testlist = totalresults(1,3,5)    
+#testlist = []
+testlist = totalresults(1,3,5)
 #%%
 
 
 def totals(blu,red,blk):
-    testlist = totalresults(blu,red,blk)   
-    totalhits = testlist.count('hit')+2*testlist.count('2hits')+testlist.count('hitcrit') 
-    totalcrits = testlist.count('crit')+testlist.count('hitcrit') 
+    testlist = totalresults(blu,red,blk)
+    totalhits = testlist.count('hit')+2*testlist.count('2hits')+testlist.count('hitcrit')
+    totalcrits = testlist.count('crit')+testlist.count('hitcrit')
     totalblanks = testlist.count('blnk')
-    totalaccuracy = testlist.count('accr') 
-    
+    totalaccuracy = testlist.count('accr')
+
     blargh = []
     blargh.append(['Hits',totalhits])
     blargh.append(['Crits',totalcrits])
     blargh.append(['Blanks',totalblanks])
     blargh.append(['Accuracy',totalaccuracy])
-    
+
     return blargh
 
-#results = totals(2,3,2)
+results = totals(2,3,2)
 #df = pd.DataFrame(results)
 #print(df)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+print(''+str(results)    )
+test = dict(results)
+print(str(test))
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -12,37 +12,44 @@ https://www.askpython.com/python-modules/flask/flask-forms
 
 from flask import Flask,render_template,request
 from flask import jsonify
-#import diceresults.py as dr
+import diceresults as dr
 import pandas as pd
 #%%
 app = Flask(__name__)
 
 @app.route("/",methods = ['GET'])
 def main():
-    return '''Welcome to Star Wars Armada Dice Calculator 
-    <br>    
+    return '''Welcome to Star Wars Armada Dice Calculator
+    <br>
     <form action="action_to_perform_after_submission" method = "POST">
         <p>Blue Dice <input type = "text" name = "BlueDice" /></p>
         <p>Red Dice <input type = "text" name = "RedDice" /></p>
         <p>Black Dice <input type = "text" name = "BlackDice" /></p>
         <p><input type = "submit" value = "submit" /></p>
-    </form> 
+    </form>
      '''
-#%%     
-#@app.route('/action_to_perform_after_submission',methods = ['GET','POST'])
+
+#%%
+@app.route('/action_to_perform_after_submission',methods = ['GET','POST'])
 
 #helpful: https://stackoverflow.com/questions/12277933/send-data-from-a-textbox-into-flask
-def action_to_perform_after_submission(): 
+def action_to_perform_after_submission():
     if request.method == 'GET':
         return f"get result"
     if request.method == 'POST':
         #results = dr.totals(2,3,2)
-        return "post results:"+str(2)
+        BlueDice = int(request.form['BlueDice'])
+        RedDice = int(request.form['RedDice'])
+        BlackDice = int(request.form['BlackDice'])
+        info = "post results: blue:"+str(BlueDice)+" red:"+str(RedDice)+" black:"+str(BlackDice)+"<br>JSON Object:"
+        fullresults = dr.totals(BlueDice,RedDice,BlackDice)
+        return info+'<br>' + str(dict(fullresults))
+
         #return f"post result: "+str(jsonify(results)) #didn't show results as a string. just put post resutls
 
 
-#%%   
+#%%
 if __name__ == "__main__":
-    app.debug = True  #remove this from final
+    app.debug = False  #False True remove this from final
     app.run(port=8000)
 
